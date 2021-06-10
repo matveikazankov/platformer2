46e45373fb58899b34cf311d9d56bd225699aae3
matveikazankov <47327054+matveikazankov@users.noreply.github.com>
2021-06-11 00:46:44 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1623361068,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bird-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/background-sheet0.png",
+		"images/bottompipe-sheet0.png",
+		"images/toppipe-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/140280b161b409e9f5bcbdaf2ad440f-sheet0.png",
+		"images/il_570xn_-sheet0.png",
+		"images/il_570xn_2-sheet0.png",
+		"images/140280b161b409e9f5bcbdaf2ad440f4_-sheet0.png",
+		"images/maxresdefault-sheet0.png",
+		"images/jj-sheet0.png",
+		"media/Для игры counter strike 1.6 (7) - Дл http__vk.com_kherson_public18 (www.hotplayer.ru).ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
